401351f30310472a4fee90446d43ce523e7e3fe4
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-26 22:15:22 +0200

    feat: add db file

---
+++ src/db/db.ts
@@ -0,0 +1,11 @@
+import { Album } from '../modules/album/entities/album.entity';
+import { Artist } from '../modules/artist/entities/artist.entity';
+import { Favorites } from '../modules/favorites/entities/favorites.entity';
+import { Track } from '../modules/track/entities/track.entity';
+import { User } from '../modules/user/entities/user.entity';
+
+export const mockAlbums: Album[] = [];
+export const mockArtists: Artist[] = [];
+export const mockFavorites: Favorites[] = [];
+export const mockTracks: Track[] = [];
+export const mockUsers: User[] = [];
