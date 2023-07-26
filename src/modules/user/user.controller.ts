d8f90c96ca4a39857950e3f0fc77cb1022df061f
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-26 22:18:02 +0200

    feat: add methods create and getUsers and get all users and post controllers in user module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { v4 } from 'uuid';
+import { mockUsers } from 'src/db/db';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  public create(CreateUserDto: CreateUserDto) {
+    const newUser: User = {
+      id: v4(),
+      login: CreateUserDto.login,
+      password: CreateUserDto.password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    mockUsers.push(newUser);
+    const { password, ...userWithNotPassword } = newUser;
+    //password
+
+    return userWithNotPassword;
+  }
+
+  public getUsers() {
+    const users = mockUsers.map(({ password, ...allWithNotPass }) => {
+      return allWithNotPass;
+    });
+    return users;
+  }
+}
