d969a7c6353c4736058d2f5d045b6801d1ceb451
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-07-30 19:06:42 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .addServer('/doc')
+    .setVersion('1.0.0')
+    .setTitle('Home Library Service')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(PORT);
 }
 bootstrap();
