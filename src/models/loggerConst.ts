83320982af23c3fdb7c90d73f52a16b8e703ca52
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-08-20 10:06:55 +0200

    feat: add variables env

--- src/models/loggerConst.ts
+++ src/models/loggerConst.ts
@@ -4,7 +4,8 @@ export default class LoggerConts {
   public static MAX_LOG_DATA_LENGTH = 1000;
 
   public static LOGGING_LEVEL = Number(process.env.LOGGING_LEVEL) || 4;
-  public static LOG_DIRECTORY = 'logs';
-  public static ERROR_LOG_FILE_NAME = 'error.log';
-  public static LOG_FILE_NAME = 'log.log';
+  public static LOG_DIRECTORY = process.env.LOG_DIRECTORY;
+  public static ERROR_LOG_FILE_NAME = process.env.ERROR_LOG_FILE_NAME;
+  public static LOG_FILE_NAME = process.env.LOG_FILE_NAME;
+  public static WARNING_LOG_FILE_NAME = process.env.WARNING_LOG_FILE_NAME;
 }
