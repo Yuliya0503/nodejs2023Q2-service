93041b1fb192b42ba242f3356b68e6fbad3dc8f1
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-08-21 17:00:21 +0200

    refactor: change using doc file and refactor code

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { LogService } from './modules/log/log.service';
-import { HttpExceptionFilter } from './modules/exceptions/http-exception-filter';
+import { load } from 'js-yaml';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { serve, setup } from 'swagger-ui-express';
 
-const PORT = Number(process.env.PORT);
-
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
-    logger: new LogService(),
     bodyParser: true,
     rawBody: true,
+    bufferLogs: true,
   });
-  app.useGlobalFilters(new HttpExceptionFilter());
+
+  configureApp(app);
+  await app.listen(process.env.PORT);
+}
+
+async function configureApp(app: INestApplication): Promise<void> {
+  app.useLogger(app.get(LogService));
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
-    .addServer('/doc')
-    .setVersion('1.0.0')
-    .setTitle('Home Library Service')
-    .setDescription('Home music library service')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
-  await app.listen(PORT);
+
+  const apiDocsPath: string = join(dirname(__dirname), 'doc', 'api.yaml');
+  const doc: unknown = load((await readFile(apiDocsPath, 'utf-8')).toString());
+
+  setupSwaggerUI(app, doc);
+}
+
+function setupSwaggerUI(app: INestApplication, doc: unknown): void {
+  app.use('/doc', serve, setup(doc));
 }
 
 bootstrap();
