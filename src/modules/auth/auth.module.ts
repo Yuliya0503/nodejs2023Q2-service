91d4ece6d394875cf3c314759311aa590c632dcb
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-08-21 12:30:53 +0200

    feat: add guard to auth. module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from '../guards/auth.guard';
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [JwtService, AuthService],
+  providers: [
+    JwtService,
+    AuthService,
+    { useClass: AuthGuard, provide: APP_GUARD },
+  ],
 })
 export class AuthModule {}
