713b6c35bfe1977c25b55cda5e976a03ed45d9c3
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-08-21 17:34:19 +0200

    refactor: change app module and change exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './modules/exceptions/http-exception-filter';
 import { UserModule } from './modules/user/user.module';
 import { AlbumModule } from './modules/album/album.module';
 import { ArtistModule } from './modules/artist/artist.module';
 import { FavoritesModule } from './modules/favorites/favorites.module';
 import { TrackModule } from './modules/track/track.module';
 import { LogModule } from './modules/log/log.module';
-import { UserService } from './modules/user/user.service';
-import { ArtistService } from './modules/artist/artist.service';
-import { AlbumService } from './modules/album/album.service';
-import { TrackService } from './modules/track/track.service';
-import { FavoritesService } from './modules/favorites/favorites.service';
-import { LogService } from './modules/log/log.service';
-import { JwtService } from '@nestjs/jwt';
-import { LoggerMiddleware } from './modules/middleware/log.middleware';
 import { AuthModule } from './modules/auth/auth.module';
-import { AuthService } from './modules/auth/auth.service';
+import { LoggerMiddleware } from './modules/middleware/log.middleware';
 
 @Module({
   imports: [
@@ -27,18 +21,14 @@ import { AuthService } from './modules/auth/auth.service';
     AuthModule,
   ],
   providers: [
-    UserService,
-    ArtistService,
-    AlbumService,
-    TrackService,
-    FavoritesService,
-    LogService,
-    JwtService,
-    AuthService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
   ],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
+  configure(consumer: MiddlewareConsumer): void {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
--- src/modules/exceptions/http-exception-filter.ts
+++ src/modules/exceptions/http-exception-filter.ts
@@ -5,19 +5,30 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { LogService } from '../log/log.service';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  constructor(private readonly loggerService: LogService) {}
+  async catch(exception: HttpException, host: ArgumentsHost): Promise<void> {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-
-    response.status(status).json({
-      path: request.url,
-      StatusCode: status,
-      timestamp: new Date().toISOString(),
-    });
+    const status: number = exception.getStatus();
+    const message: string = exception.message || 'Server Error';
+    const errorDataLog = {
+      request: {
+        method: request.method,
+        url: request.url,
+        body: request.body,
+        query: request.query,
+      },
+      response: {
+        message,
+        StatusCode: status,
+      },
+    };
+    await this.loggerService.error(JSON.stringify(errorDataLog));
+    response.status(status).json(errorDataLog);
   }
 }
